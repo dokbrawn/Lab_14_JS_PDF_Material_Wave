9d72da0bb750be503743132970a70d32ccb36a05
dokbrawn <tanisimov2006@gmail.com>
2025-05-21 02:07:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,22 +3,47 @@ import javascriptLogo from '../javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { jsPDF } from "jspdf";
+
+document.querySelectorAll('[contenteditable]').forEach(el => {
+  el.addEventListener('input', () => {
+    localStorage.setItem(el.className || el.id, el.innerHTML);
+  });
+
+  const saved = localStorage.getItem(el.className || el.id);
+  if (saved) el.innerHTML = saved;
+});
+
+document.getElementById('download-pdf').addEventListener('click', () => {
+  const pdf = new jsPDF();
+  const content = document.querySelector('.resume-container');
+
+  pdf.html(content, {
+    callback: function (doc) {
+      doc.save("resume.pdf");
+    },
+    x: 10,
+    y: 10,
+    width: 180
+  });
+});
+
+document.querySelectorAll('[data-ripple]').forEach(el => {
+  el.addEventListener('click', function (e) {
+    const circle = document.createElement('span');
+    const diameter = Math.max(el.clientWidth, el.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${e.clientX - el.getBoundingClientRect().left - radius}px`;
+    circle.style.top = `${e.clientY - el.getBoundingClientRect().top - radius}px`;
+    circle.classList.add('ripple');
+
+    const ripple = el.getElementsByClassName('ripple')[0];
+    if (ripple) ripple.remove();
+
+    el.appendChild(circle);
+  });
+});
 
 setupCounter(document.querySelector('#counter'))
